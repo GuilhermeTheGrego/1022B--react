da649c156d71380a68fb17b54736163e1f23e87b
aluno <aluno@NV-LAB02-135534.ifms.edu.br>
2024-03-11 11:24:27 -0400

    meu primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+import './App.css'
+function App() {
+  
+ let nome = "Guilherme"
+ let sobrenome = "De Oliveira Souza"
+  return (
+ <>
+     <h1 className="nome">Nome:{nome} </h1>
+     <p className="sobrenome">{sobrenome}</p>
+     </>
+  )// retorna jsx - javascript XMl
+}
+
+export default App
